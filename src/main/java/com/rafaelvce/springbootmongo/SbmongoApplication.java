b9118668471e30fbd6a7839114098f2142a7ede0
Rafael Ceconi <rafaelvce@gmail.com>
2020-06-13 17:46:08 -0300

    Firts commit

---
+++ src/main/java/com/rafaelvce/springbootmongo/SbmongoApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelvce.springbootmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelvce/springbootmongo/SbmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelvce.springbootmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
