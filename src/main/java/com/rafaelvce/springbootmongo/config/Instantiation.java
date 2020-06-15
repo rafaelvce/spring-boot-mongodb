0101776e542c016d63ac022ed2bd292355ab1887
Rafael Ceconi <rafaelvce@gmail.com>
2020-06-15 13:44:46 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/rafaelvce/springbootmongo/config/Instantiation.java
+++ src/main/java/com/rafaelvce/springbootmongo/config/Instantiation.java
@@ -1,5 +1,6 @@
 package com.rafaelvce.springbootmongo.config;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.TimeZone;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import com.rafaelvce.springbootmongo.domain.Post;
 import com.rafaelvce.springbootmongo.domain.User;
 import com.rafaelvce.springbootmongo.dto.AuthorDTO;
+import com.rafaelvce.springbootmongo.dto.CommentDTO;
 import com.rafaelvce.springbootmongo.respositories.PostRepository;
 import com.rafaelvce.springbootmongo.respositories.UserRepository;
 
@@ -40,6 +42,13 @@ public class Instantiation implements CommandLineRunner{
 
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
--- src/main/java/com/rafaelvce/springbootmongo/domain/Post.java
+++ src/main/java/com/rafaelvce/springbootmongo/domain/Post.java
@@ -1,24 +1,29 @@
 package com.rafaelvce.springbootmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.rafaelvce.springbootmongo.dto.AuthorDTO;
+import com.rafaelvce.springbootmongo.dto.CommentDTO;
 
 @Document
-public class Post implements Serializable{
+public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	
+
+	private List<CommentDTO> comments = new ArrayList<>();
+
 	public Post() {
 	}
 
@@ -62,13 +67,21 @@ public class Post implements Serializable{
 	public void setBody(String body) {
 		this.body = body;
 	}
-	
+
 	public AuthorDTO getAuthor() {
 		return author;
 	}
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
